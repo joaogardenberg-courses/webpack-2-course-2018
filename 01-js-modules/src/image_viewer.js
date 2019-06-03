cd1b9bb8e275ec23f5ce84a1d9748b7b7c53e5f8
João Gardenberg <joaogardenberg@gmail.com>
2019-06-03 18:49:45 -0300

    Initial commit

---
+++ 01-js-modules/src/image_viewer.js
@@ -0,0 +1,9 @@
+import small from '../assets/small.jpg';
+import '../styles/image_viewer.css';
+
+export default () => {
+  const smallImage = document.createElement('img');
+  smallImage.src = small;
+
+  document.body.appendChild(smallImage);
+};
